1d2e0145fd6aeb56e25c8b799e1de155b5f0d423
Riya Patil <riyadpatil7@gmail.com>
2026-05-03 14:17:50 +0530

    First Commit

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+// Save this file as HelloWorld.java
+public class HelloWorld {
+    public static void main(String[] args) {
+        // This line prints the message to the console
+        System.out.println("Hello, World!");
+    }
+}
